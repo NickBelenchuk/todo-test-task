687fba07533789bda7a23bc956638a82ff6bb563
Nick <nick.belenchuk@gmail.com>
2023-12-20 14:23:32 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ListItem } from "./components/ListItem";
+import { InputTask } from "./components/InputTask";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      text: "Create Todo",
+      complete: true,
+    },
+    {
+      text: "Create/delete tasks",
+      complete: true,
+    },
+    {
+      text: "Animation",
+      complete: false,
+    },
+  ]);
+
+  const onToggleComplete = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, curIdx) =>
+        index === curIdx
+          ? {
+              ...task,
+              complete: !task.complete,
+            }
+          : task
+      )
+    );
+  };
+
+  const onRemoveTask = (index) => {
+    setTasks((prev) => prev.filter((_, curIdx) => index !== curIdx));
+  };
+
+  const onAddTask = (text) => {
+    setTasks((prev) => [
+      ...prev,
+      {
+        text,
+        complete: false,
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__header">
+        <h4>My ToDo</h4>
+      </div>
+      <InputTask onAddTask={onAddTask} />
+      <div className="todo__list">
+        {tasks.map((task, index) => (
+          <ListItem
+            key={index}
+            index={index}
+            text={task.text}
+            complete={task.complete}
+            onToggleComplete={onToggleComplete}
+            onRemoveTask={onRemoveTask}
+          />
+        ))}
+      </div>
     </div>
   );
 }
